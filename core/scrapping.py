17524646f56df038c0e2eae94fddacd229c3131c
Diego Nascimento <df.nascimento93@gmail.com>
2024-12-24 00:54:59 -0300

    Atualizações 23/12 2

--- core/scrapping.py
+++ core/scrapping.py
@@ -14,24 +14,22 @@ def scrap_producao(ano):
 
         itens = soup.find_all('td', class_=['tb_item','tb_subitem'])
 
-        df_itens = pd.DataFrame([], columns=['Produto','Tipo','Quantidade[L.]', 'Ano'])
-        
-        print(len(itens))
+        lista = []
 
-        tipo = ''
 
         for i in range(0, len(itens), 2):
             produto = itens[i].text.strip()
             quantidade = itens[i+1].text.strip()
 
-            if (produto.upper()):
+            if (produto.isupper()):
                 tipo = produto
 
-            df_itens.loc[df_itens] = [produto, tipo, quantidade, ano]
+            lista.append([produto, tipo, quantidade, ano])
 
+        df_itens = pd.DataFrame(lista, columns=['Produto','Tipo','Quantidade[L.]', 'Ano'])
         print(df_itens)
     except Exception as e:
         print("Erro" + str(e))
 
 
-scrap_producao(2021)
+scrap_producao(127)
